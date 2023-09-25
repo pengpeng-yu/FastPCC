06b0c23675bf63f0196a11bbdf2b7a1cba60d15e
pengpeng-yu <2649535611@qq.com>
2023-09-25 23:39:54 +0800

    Misc.

---
+++ lib/datasets/PlyVoxel/MPEG_GPCC_CTC_Solid_MVUB.py
@@ -0,0 +1,19 @@
+from lib.datasets.PlyVoxel.Base import Dataset
+from lib.datasets.PlyVoxel.Base import Config as BaseDatasetConfig
+from dataclasses import dataclass
+from typing import Tuple, Union
+
+
+@dataclass
+class Config(BaseDatasetConfig):
+    root: Union[str, Tuple[str, ...]] = (
+        'datasets/MPEG_GPCC_CTC/Solid', 'datasets/MPEG_GPCC_CTC/Solid', 'datasets/MPEG_GPCC_CTC/Solid',
+        'datasets/MVUB'
+    )
+    filelist_path: Union[str, Tuple[str, ...]] = (
+        'Solid_1024.txt', 'Solid_2048.txt', 'Solid_4096.txt',
+        'list.txt'
+    )
+
+    ori_resolution: Union[int, Tuple[int, ...]] = (1024, 2048, 4096, 512)
+    resolution: Union[int, Tuple[int, ...]] = (1024, 2048, 4096, 512)
